b9ec7c2788b9e4af9fba99efa98f6d2f3ed8b9ad
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-02-24 16:57:28 +0800

    setup form with input save button cancel button

--- Main.elm
+++ Main.elm
@@ -1,7 +1,91 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
+-- model
+
+
+type alias Model =
+    { players : List Player
+    , name : String
+    , playerId : Maybe Int
+    , plays : List Play
+    }
+
+
+type alias Player =
+    { id : Int
+    , name : String
+    , points : Int
+    }
+
+
+type alias Play =
+    { id : Int
+    , playerId : Int
+    , name : Int
+    , points : Int
+    }
+
+
+initModel : Model
+initModel =
+    { players = []
+    , name = ""
+    , playerId = Nothing
+    , plays = []
+    }
+
+
+type Msg
+    = Edit Player
+    | Score Player Int
+    | Input String
+    | Save
+    | Cancel
+    | DeletePlay Play
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Input name ->
+            { model | name = name }
+
+        _ ->
+            model
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "scoreboard" ]
+        [ h1 [] [ text "Skore" ]
+        , playerForm model
+        ]
+
+
+playerForm : Model -> Html Msg
+playerForm model =
+    Html.form [ onSubmit Save ]
+        [ input
+            [ type_ "text"
+            , placeholder "Add/Edit Player..."
+            , onInput Input
+            , value model.name
+            ]
+            []
+        , button [ type_ "submit" ] [ text "Save" ]
+        , button [ type_ "cancel", onClick Cancel ] [ text "Cancel" ]
+        ]
+
+
+main : Program Never Model Msg
 main =
-    text "Skore!"
+    Html.beginnerProgram
+        { model = initModel
+        , view = view
+        , update = update
+        }
