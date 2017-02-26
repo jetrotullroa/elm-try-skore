d60b0682ce7fc48457ce5c1e1fe083828ca95bae
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-02-26 10:47:01 +0800

    add function for add points and edit player

--- Main.elm
+++ Main.elm
@@ -64,8 +64,35 @@ update msg model =
             else
                 save model
 
+        Score player points ->
+            score model player points
+
+        Edit player ->
+            { model | name = player.name, playerId = Just player.id }
+
         _ ->
             model
+
+
+score : Model -> Player -> Int -> Model
+score model scorer points =
+    let
+        newPlayers =
+            List.map
+                (\player ->
+                    if player.id == scorer.id then
+                        { player
+                            | points = player.points + points
+                        }
+                    else
+                        player
+                )
+                model.players
+
+        play =
+            Play (List.length model.plays) scorer.id scorer.name points
+    in
+        { model | players = newPlayers, plays = play :: model.plays }
 
 
 save : Model -> Model
@@ -155,7 +182,11 @@ playerListHeader =
 
 playerList : Model -> Html Msg
 playerList model =
-    ul [] (List.map player model.players)
+    -- ul [] (List.map player model.players)
+    model.players
+        |> List.sortBy .name
+        |> List.map player
+        |> ul []
 
 
 player : Player -> Html Msg
